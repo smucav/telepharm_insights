99a341f567cfaf2815b003050e3d151d10d19095
Daniel Tujuma <smucav@gmail.com>
2025-07-12 13:36:35 +0300

    add: data build tool for telegram message

---
+++ dbt/telepharm_dbt/models/marts/fct_messages.sql
@@ -0,0 +1,17 @@
+{{ config(
+    materialized='table',
+    schema='marts'
+) }}
+
+SELECT
+    m.message_id,
+    c.channel_id,
+    m.message_date::DATE AS date_id,
+    m.text,
+    m.has_image,
+    m.image_file,
+    m.message_length,
+    m.load_timestamp
+FROM {{ ref('stg_telegram_messages') }} m
+JOIN {{ ref('dim_channels') }} c ON m.channel = c.channel_name
+JOIN {{ ref('dim_dates') }} d ON m.message_date::DATE = d.date_id
